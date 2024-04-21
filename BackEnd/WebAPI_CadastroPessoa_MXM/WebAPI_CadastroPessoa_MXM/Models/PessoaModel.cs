d54cb199ac5a964e4e40b886192818c94bb49fb2
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-04-21 16:54:26 -0300

    back end: web API cadastro de pessoas - testado com swagger

---
+++ BackEnd/WebAPI_CadastroPessoa_MXM/WebAPI_CadastroPessoa_MXM/Models/PessoaModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_CadastroPessoa_MXM.Modelos
+{
+    public class PessoaModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public bool StatusCadastro { get; set; }
+        public DateTime DataCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataAlteracao { get; set; } = DateTime.Now.ToLocalTime();
+
+    }
+}

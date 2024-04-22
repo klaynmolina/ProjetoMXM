5825684d1f6167c4c3a5561bf489ee55d62bee91
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-04-22 19:03:59 -0300

    Ajuste CORS POLICY

---
+++ BackEnd/WebAPI_CadastroPessoa_MXM/DB/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_CadastroPessoa_MXM.Modelos;
+
+namespace WebAPI_CadastroPessoa_MXM.DB
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<PessoaModel> Pessoas { get; set;}
+    }
+}

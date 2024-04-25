6d450512a669e7d4f6db778131478154e60d066f
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-04-24 22:49:06 -0300

    Adição de rota

--- FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/app-routing.module.ts
+++ FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { EnderecoComponent } from './Components/endereco/endereco.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: '/', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'atualizar/:id', component: AtualizarComponent},

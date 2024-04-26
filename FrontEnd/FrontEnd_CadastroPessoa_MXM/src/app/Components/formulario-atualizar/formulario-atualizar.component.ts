040acea6600ed6fa9fea94d99824d9891bfe8918
Vanessa Molina <vanessakrmolina@hotmail.com>
2024-04-26 16:47:44 -0300

    Implementação de máscara de CPF e Telefone na página atualizar e otimização de botões repetidos

--- FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/Components/formulario-atualizar/formulario-atualizar.component.ts
+++ FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/Components/formulario-atualizar/formulario-atualizar.component.ts
@@ -11,10 +11,9 @@ import { DuplicidadeModalComponent } from '../duplicidade-modal/duplicidade-moda
 @Component({
   selector: 'app-formulario-atualizar',
   templateUrl: './formulario-atualizar.component.html',
-  styleUrls: ['./formulario-atualizar.component.css']
+  styleUrls: ['./formulario-atualizar.component.css'],
 })
 export class FormularioAtualizarComponent {
-
   @Output() onSubmit = new EventEmitter<Pessoa>();
   @Input() btnFuncaoAtualizar: string = 'Atualizar Cadastro';
   @Input() dadosPessoa: Pessoa | null = null;
@@ -25,47 +24,71 @@ export class FormularioAtualizarComponent {
   constructor(
     private viaCepService: ViaCepService,
     private pessoaService: PessoaService,
-    public dialog: MatDialog,
-  ) { }
-  
+    public dialog: MatDialog
+  ) {}
+
   ngOnInit(): void {
-    // console.log(this.dadosPessoa);
     this.formularioDados = new FormGroup({
       id: new FormControl(this.dadosPessoa ? this.dadosPessoa.id : 0),
-      nome: new FormControl(this.dadosPessoa ? this.dadosPessoa.nome : '', [Validators.required, Validators.minLength(3)]),
-      documento: new FormControl(this.dadosPessoa ? this.dadosPessoa.documento : '', [Validators.required, Validators.minLength(11)]),
-      email: new FormControl(this.dadosPessoa ? this.dadosPessoa.email : '', [Validators.required, Validators.email]),
-      telefone: new FormControl(this.dadosPessoa ? this.dadosPessoa.telefone : '', [Validators.required, Validators.minLength(8)]),
-      endereco: new FormControl(this.dadosPessoa ? this.dadosPessoa.endereco : '', [Validators.required]),
-      statusCadastro: new FormControl(this.dadosPessoa ? this.dadosPessoa.statusCadastro : true),
+      nome: new FormControl(this.dadosPessoa ? this.dadosPessoa.nome : '', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      documento: new FormControl(
+        this.dadosPessoa
+          ? this.dadosPessoa.documento.length <= 11
+            ? this.formatCpf(this.dadosPessoa.documento)
+            : this.formatCnpj(this.dadosPessoa.documento)
+          : '',
+        [Validators.required, Validators.minLength(11)]
+      ),
+      email: new FormControl(this.dadosPessoa ? this.dadosPessoa.email : '', [
+        Validators.required,
+        Validators.email,
+      ]),
+      telefone: new FormControl(
+        this.dadosPessoa
+          ? this.dadosPessoa.telefone.length == 8
+            ? this.formatTelefone(this.dadosPessoa.telefone)
+            : this.formatCelular(this.dadosPessoa.telefone)
+          : '',
+        [Validators.required, Validators.minLength(8)]
+      ),
+      endereco: new FormControl(
+        this.dadosPessoa ? this.dadosPessoa.endereco : '',
+        [Validators.required]
+      ),
+      statusCadastro: new FormControl(
+        this.dadosPessoa ? this.dadosPessoa.statusCadastro : true
+      ),
       dataCriacao: new FormControl(new Date()),
       dataAlteracao: new FormControl(new Date()),
-    });   
+    });
   }
 
   async atualizar() {
     const enderecoCompleto = this.formularioDados.value.endereco;
-    const somenteCEP = enderecoCompleto.slice(-9).replace(/-/g, "");
+    const somenteCEP = enderecoCompleto.slice(-9).replace(/-/g, '');
 
     const email = this.formularioDados.value.email;
-    const documento = this.formularioDados.value.documento;   
+    const documento = this.formularioDados.value.documento;
 
     let testeEmail = await this.validarDuplicidadeEmail(email);
     let testeDocumento = await this.validarDuplicidadeDocumento(documento);
-    
-    if(testeEmail || testeDocumento) {
+
+    if (testeEmail || testeDocumento) {
       console.log(123);
-      
+
       this.DuplicidadeDialog();
       return;
     }
 
     const cep = this.formularioDados.value.endereco;
-    if(cep != null) {
+    if (cep != null) {
       try {
         const cep = somenteCEP;
         this.endereco = await this.viaCepService.buscarEndereco(cep);
-        if(this.endereco == 'true') {
+        if (this.endereco == 'true') {
           this.CepDialog();
           return;
         }
@@ -73,66 +96,99 @@ export class FormularioAtualizarComponent {
       } catch (error) {
         this.CepDialog();
         return;
-      }   
-    }    
-      
+      }
+    }
+
     this.onSubmit.emit(this.formularioDados.value);
-    // console.log(this.formularioDados.value);  
-  }  
+    // console.log(this.formularioDados.value);
+  }
 
   DuplicidadeDialog() {
     this.dialog.open(DuplicidadeModalComponent, {
       width: '450px',
     });
-  } 
+  }
 
   CepDialog() {
     this.dialog.open(CepModalComponent, {
       width: '450px',
     });
-  }  
+  }
 
   async validarDuplicidadeEmail(email: string) {
-      const cadastrosResponse = await firstValueFrom(this.pessoaService.GetPessoas());
-      const resposta = cadastrosResponse.dados; 
-      
-      if (!resposta) {
-          return false; // Retorna false se os dados forem indefinidos
-      }
+    const cadastrosResponse = await firstValueFrom(
+      this.pessoaService.GetPessoas()
+    );
+    const resposta = cadastrosResponse.dados;
 
-      const cadastros: Pessoa[] = resposta;
-
-      // Verifica se algum dos cadastros tem o email desejado
-      return cadastros.some((pessoa: Pessoa) => pessoa.email === email && pessoa.id != this.formularioDados.value.id);
+    if (!resposta) {
+      return false; // Retorna false se os dados forem indefinidos
     }
 
-    async validarDuplicidadeDocumento(documento: string) {
-      const cadastrosResponse = await firstValueFrom(this.pessoaService.GetPessoas());
-      const resposta = cadastrosResponse.dados; 
+    const cadastros: Pessoa[] = resposta;
 
-      if (!resposta) {
-          return false; // Retorna false se os dados forem indefinidos
-      }
+    // Verifica se algum dos cadastros tem o email desejado
+    return cadastros.some(
+      (pessoa: Pessoa) =>
+        pessoa.email === email && pessoa.id != this.formularioDados.value.id
+    );
+  }
 
-      const cadastros: Pessoa[] = resposta;
+  async validarDuplicidadeDocumento(documento: string) {
+    const cadastrosResponse = await firstValueFrom(
+      this.pessoaService.GetPessoas()
+    );
+    const resposta = cadastrosResponse.dados;
 
-      // Verifica se algum dos cadastros tem o documento desejado
-      return cadastros.some((pessoa: Pessoa) => pessoa.documento === documento && pessoa.id != this.formularioDados.value.id);
-    }
-    
-    onPaste(event: ClipboardEvent): void {
-      event.preventDefault(); // Impede a ação padrão de colagem
-  
-      // Obtém os dados colados
-      navigator.clipboard.readText().then(pastedData => {
-        // Remove caracteres especiais usando uma expressão regular
-        const sanitizedText = pastedData.replace(/[^\w\s]/gi, '');
-  
-        // Obtém a referência do campo de entrada e define o valor com os dados sanitizados
-        const input = event.target as HTMLInputElement;
-        input.value = sanitizedText;
-        this.formularioDados.value.documento = sanitizedText;
-      });
+    if (!resposta) {
+      return false; // Retorna false se os dados forem indefinidos
     }
 
+    const cadastros: Pessoa[] = resposta;
+
+    // Verifica se algum dos cadastros tem o documento desejado
+    return cadastros.some(
+      (pessoa: Pessoa) =>
+        pessoa.documento === documento &&
+        pessoa.id != this.formularioDados.value.id
+    );
+  }
+
+  onPaste(event: ClipboardEvent): void {
+    event.preventDefault(); // Impede a ação padrão de colagem
+
+    // Obtém os dados colados
+    navigator.clipboard.readText().then((pastedData) => {
+      // Remove caracteres especiais usando uma expressão regular
+      const sanitizedText = pastedData.replace(/[^\w\s]/gi, '');
+
+      // Obtém a referência do campo de entrada e define o valor com os dados sanitizados
+      const input = event.target as HTMLInputElement;
+      input.value = sanitizedText;
+      this.formularioDados.value.documento = sanitizedText;
+    });
+  }
+
+  // Máscara CPF
+  private formatCpf(value: string): string {
+    return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  }
+
+  // Máscara CNPJ
+  private formatCnpj(value: string): string {
+    return value.replace(
+      /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+      '$1.$2.$3/$4-$5'
+    );
+  }
+
+  // Máscara Telefone Fixo
+  private formatTelefone(value: string): string {
+    return value.replace(/^(\d{4})(\d{4})$/, '$1-$2');
+  }
+
+  // Máscara Celular
+  private formatCelular(value: string): string {
+    return value.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+  }
 }
--- FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/Components/formulario-cadastro/formulario-cadastro.component.ts
+++ FrontEnd/FrontEnd_CadastroPessoa_MXM/src/app/Components/formulario-cadastro/formulario-cadastro.component.ts
@@ -11,11 +11,9 @@ import { DuplicidadeModalComponent } from '../duplicidade-modal/duplicidade-moda
 @Component({
   selector: 'app-formulario-cadastro',
   templateUrl: './formulario-cadastro.component.html',
-  styleUrls: ['./formulario-cadastro.component.css']
+  styleUrls: ['./formulario-cadastro.component.css'],
 })
-
-export class FormularioCadastroComponent {  
-
+export class FormularioCadastroComponent {
   @Output() onSubmit = new EventEmitter<Pessoa>();
   @Input() btnFuncaoCadastrar: string = 'Cadastrar';
   @Input() dadosPessoa: Pessoa | null = null;
@@ -26,110 +24,162 @@ export class FormularioCadastroComponent {
   constructor(
     private viaCepService: ViaCepService,
     private pessoaService: PessoaService,
-    public dialog: MatDialog,
-  ) { }
-  
+    public dialog: MatDialog
+  ) {}
+
   ngOnInit(): void {
-    // console.log(this.dadosPessoa);
     this.formularioDados = new FormGroup({
       id: new FormControl(this.dadosPessoa ? this.dadosPessoa.id : 0),
-      nome: new FormControl(this.dadosPessoa ? this.dadosPessoa.nome : '', [Validators.required, Validators.minLength(3)]),
-      documento: new FormControl(this.dadosPessoa ? this.dadosPessoa.documento : '', [Validators.required, Validators.minLength(11)]),
-      email: new FormControl(this.dadosPessoa ? this.dadosPessoa.email : '', [Validators.required, Validators.email]),
-      telefone: new FormControl(this.dadosPessoa ? this.dadosPessoa.telefone : '', [Validators.required, Validators.minLength(8)]),
-      endereco: new FormControl(this.dadosPessoa ? this.dadosPessoa.endereco : '', [Validators.required]),
-      statusCadastro: new FormControl(this.dadosPessoa ? this.dadosPessoa.statusCadastro : true),
+      nome: new FormControl(this.dadosPessoa ? this.dadosPessoa.nome : '', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      documento: new FormControl(
+        this.dadosPessoa
+          ? this.dadosPessoa.documento.length <= 11
+            ? this.formatCpf(this.dadosPessoa.documento)
+            : this.formatCnpj(this.dadosPessoa.documento)
+          : '',
+        [Validators.required, Validators.minLength(11)]
+      ),
+      email: new FormControl(this.dadosPessoa ? this.dadosPessoa.email : '', [
+        Validators.required,
+        Validators.email,
+      ]),
+      telefone: new FormControl(
+        this.dadosPessoa
+          ? this.dadosPessoa.telefone.length == 8
+            ? this.formatTelefone(this.dadosPessoa.telefone)
+            : this.formatCelular(this.dadosPessoa.telefone)
+          : '',
+        [Validators.required, Validators.minLength(8)]
+      ),
+      endereco: new FormControl(
+        this.dadosPessoa ? this.dadosPessoa.endereco : '',
+        [Validators.required]
+      ),
+      statusCadastro: new FormControl(
+        this.dadosPessoa ? this.dadosPessoa.statusCadastro : true
+      ),
       dataCriacao: new FormControl(new Date()),
       dataAlteracao: new FormControl(new Date()),
-    });   
+    });
   }
 
-  async cadastrar() {    
+  async cadastrar() {
     const email = this.formularioDados.value.email;
-    const documento = this.formularioDados.value.documento;    
+    const documento = this.formularioDados.value.documento;
     try {
       const cep = this.formularioDados.value.endereco;
       this.endereco = await this.viaCepService.buscarEndereco(cep);
-      if(this.endereco == 'true') {
+      if (this.endereco == 'true') {
         this.CepDialog();
         return;
       }
-      
+
       let testeEmail = await this.validarDuplicidadeEmail(email);
       let testeDocumento = await this.validarDuplicidadeDocumento(documento);
-      
-      if( testeEmail || testeDocumento) {
+
+      if (testeEmail || testeDocumento) {
         this.DuplicidadeDialog();
         return;
       }
-      
+
       this.formularioDados.value.endereco = this.endereco;
     } catch (error) {
       this.CepDialog();
       return;
-    }   
+    }
     this.onSubmit.emit(this.formularioDados.value);
-    // console.log(this.formularioDados.value);  
-  }  
+    // console.log(this.formularioDados.value);
+  }
 
   CepDialog() {
     this.dialog.open(CepModalComponent, {
       width: '450px',
     });
-  }  
+  }
 
   DuplicidadeDialog() {
     this.dialog.open(DuplicidadeModalComponent, {
       width: '450px',
     });
-  } 
+  }
 
-    async validarDuplicidadeEmail(email: string) {
-      const cadastrosResponse = await firstValueFrom(this.pessoaService.GetPessoas());
-      const resposta = cadastrosResponse.dados; 
-      
-      if (!resposta) {
-          return false; // Retorna false se os dados forem indefinidos
-      }
+  async validarDuplicidadeEmail(email: string) {
+    const cadastrosResponse = await firstValueFrom(
+      this.pessoaService.GetPessoas()
+    );
+    const resposta = cadastrosResponse.dados;
 
-      const cadastros: Pessoa[] = resposta;
-
-      // Verifica se algum dos cadastros tem o email desejado
-      return cadastros.some((pessoa: Pessoa) => pessoa.email === email);
+    if (!resposta) {
+      return false; // Retorna false se os dados forem indefinidos
     }
 
-    async validarDuplicidadeDocumento(documento: string) {
-      const cadastrosResponse = await firstValueFrom(this.pessoaService.GetPessoas());
-      const resposta = cadastrosResponse.dados;
+    const cadastros: Pessoa[] = resposta;
 
-      if (!resposta) {
-          return false; // Retorna false se os dados forem indefinidos
-      }
+    // Verifica se algum dos cadastros tem o email desejado
+    return cadastros.some((pessoa: Pessoa) => pessoa.email === email);
+  }
 
-      const cadastros: Pessoa[] = resposta;
+  async validarDuplicidadeDocumento(documento: string) {
+    const cadastrosResponse = await firstValueFrom(
+      this.pessoaService.GetPessoas()
+    );
+    const resposta = cadastrosResponse.dados;
 
-      // Verifica se algum dos cadastros tem o documento desejado
-      return cadastros.some((pessoa: Pessoa) => pessoa.documento === documento);
+    if (!resposta) {
+      return false; // Retorna false se os dados forem indefinidos
     }
 
-    onPaste(event: ClipboardEvent): void {
-      event.preventDefault(); // Impede a ação padrão de colagem
-  
-      // Obtém os dados colados
-      navigator.clipboard.readText().then(pastedData => {
-        // Remove caracteres especiais e espaços usando uma expressão regular
-        const sanitizedText = pastedData.replace(/[^\w\s]/gi, '').replace(/\s+/g, '');
-  
-        // Obtém a referência do campo de entrada e define o valor com os dados sanitizados
-        const input = event.target as HTMLInputElement;
-        input.value = sanitizedText;
-  
-        // Adiciona uma pequena pausa antes de remover as classes de validação
-        setTimeout(() => {
-          input.dispatchEvent(new Event('input')); // Dispara um evento de entrada para acionar a validação
-          input.dispatchEvent(new Event('blur')); // Dispara um evento de desfoque para forçar a validação
-        });
+    const cadastros: Pessoa[] = resposta;
+
+    // Verifica se algum dos cadastros tem o documento desejado
+    return cadastros.some((pessoa: Pessoa) => pessoa.documento === documento);
+  }
+
+  onPaste(event: ClipboardEvent): void {
+    event.preventDefault(); // Impede a ação padrão de colagem
+
+    // Obtém os dados colados
+    navigator.clipboard.readText().then((pastedData) => {
+      // Remove caracteres especiais e espaços usando uma expressão regular
+      const sanitizedText = pastedData
+        .replace(/[^\w\s]/gi, '')
+        .replace(/\s+/g, '');
+
+      // Obtém a referência do campo de entrada e define o valor com os dados sanitizados
+      const input = event.target as HTMLInputElement;
+      input.value = sanitizedText;
+
+      // Adiciona uma pequena pausa antes de remover as classes de validação
+      setTimeout(() => {
+        input.dispatchEvent(new Event('input')); // Dispara um evento de entrada para acionar a validação
+        input.dispatchEvent(new Event('blur')); // Dispara um evento de desfoque para forçar a validação
       });
-    }
+    });
+  }
 
+  // Máscara CPF
+  private formatCpf(value: string): string {
+    return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  }
+
+  // Máscara CNPJ
+  private formatCnpj(value: string): string {
+    return value.replace(
+      /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+      '$1.$2.$3/$4-$5'
+    );
+  }
+
+  // Máscara Telefone Fixo
+  private formatTelefone(value: string): string {
+    return value.replace(/^(\d{4})(\d{4})$/, '$1-$2');
+  }
+
+  // Máscara Celular
+  private formatCelular(value: string): string {
+    return value.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+  }
 }
